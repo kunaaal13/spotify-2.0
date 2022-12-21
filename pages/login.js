22c50385df5d13a7d27770185d88c57ad66ab99b
Kunaaal13 <mrkunalyadav7@gmail.com>
2022-12-21 20:03:08 +0530

    login ui done

--- pages/login.js
+++ pages/login.js
@@ -6,11 +6,17 @@ function Login() {
     <div className='bg-[#03071e] w-full h-screen px-5 flex items-center justify-center text-white'>
       <div className='flex flex-col lg:flex-row items-center justify-center w-full px-5'>
         <div className='lg:flex-[0.5] flex items-center justify-center'>
-          <Image src='/images/logo.png' width={300} height={200} alt='' />
+          <Image
+            src='/images/logo.png'
+            alt=''
+            height='300'
+            width={300}
+            className='w-2/3 h-auto'
+          />
         </div>
 
         <div className='mt-10 lg:mt-0 lg:flex-[0.5] text-center lg:text-left'>
-          <h2 className='text-4xl lg:text-6xl font-extrabold leading-[1.5]'>
+          <h2 className='text-3xl sm:text-4xl md:text-5xl font-extrabold leading-snug md:leading-snug'>
             Play millions of songs and podcasts, for free.
           </h2>
 
