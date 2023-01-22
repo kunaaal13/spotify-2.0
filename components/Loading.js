827d49f56326f2a816b4fc722c986109815305a4
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-01-22 16:00:41 +0530

    loader fixes

--- components/Loading.js
+++ components/Loading.js
@@ -3,13 +3,7 @@ import React from 'react'
 function Loading() {
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center bg-black'>
-      <div class='box'>
-        <div class='ball one'></div>
-        <div class='ball two'></div>
-        <div class='ball three'></div>
-        <div class='ball four'></div>
-        <div class='ball five'></div>
-      </div>
+      <span class='loader'></span>
 
       <h3 className='mt-3 text-xl text-white'>Connecting to spotify</h3>
     </div>
