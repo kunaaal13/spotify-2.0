6cfbc4621101f3aa4d1d769a718b7be43e6f00bc
kunaaal13 <84015751+kunaaal13@users.noreply.github.com>
2023-01-22 13:11:40 +0530

    swc update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
   images: {
     domains: ['i.scdn.co', 'lh3.googleusercontent.com'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
